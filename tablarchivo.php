eb5976057e98114a00620a5cacd4c4bcd677ec6a
sleonsalome <sleonsalome@gmail.com>
2021-05-01 21:53:05 -0500

    upload

--- tablarchivo.php
+++ tablarchivo.php
@@ -2,6 +2,7 @@
     $conexion=mysqli_connect('localhost','root','','php_login_database');
 ?>
 
+
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"/>
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap5.min.css">
 <link rel="stylesheet" href="/css/bootstrap.min.css">
